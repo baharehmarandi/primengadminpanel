78a81001dab9338e38ffe0d8a49e016a2a8d0ec7
baharehmarandi <70742323+baharehmarandi@users.noreply.github.com>
2025-11-13 17:37:16 +0330

    working on sidebar: add searchbar and some items

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, input, OnDestroy, OnInit, signal} from '@angular/core';
 import {Avatar} from 'primeng/avatar';
 import {Router, RouterOutlet} from '@angular/router';
 import {Divider} from 'primeng/divider';
@@ -6,6 +6,12 @@ import {SidebarMenuComponent} from '../components/sidebar-menu/sidebar-menu.comp
 import {Menu} from 'primeng/menu';
 import {AuthService} from '../../features/auth/service/auth.service';
 import {MenuItem} from 'primeng/api';
+import {IconField} from 'primeng/iconfield';
+import {InputIcon} from 'primeng/inputicon';
+import {InputText} from 'primeng/inputtext';
+import {FormsModule} from '@angular/forms';
+import {SidebarMenu} from '../models/sidebar-menu.interface';
+import {debounceTime, distinctUntilChanged, Subject, takeUntil} from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -14,14 +20,21 @@ import {MenuItem} from 'primeng/api';
     RouterOutlet,
     Divider,
     SidebarMenuComponent,
-    Menu
+    Menu,
+    IconField,
+    InputIcon,
+    InputText,
+    FormsModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.less'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  sidebarMenu = [
+  private searchSubject = new Subject<string>();
+  private destroy$ = new Subject<void>();
+
+  sidebarMenu = signal<SidebarMenu[]>([
     {
       id: 1,
       title: 'داشبورد',
@@ -33,36 +46,100 @@ export class HeaderComponent implements OnInit {
       title: 'مدیریت کاربران',
       route: '/user-management',
       iconName:'pi-user',
+    },
+    {
+      id: 3,
+      title: 'مدیریت دسته بندی',
+      route: '/category-management',
+      iconName:'pi-folder',
+    },
+    {
+      id: 4,
+      title: 'مدیریت محصول',
+      route: '/product-management',
+      iconName:'pi-shopping-bag',
+    },
+    {
+      id: 5,
+      title: 'مدیریت فروش',
+      route: '/sell-management',
+      iconName:'pi-receipt',
+    },
+    {
+      id: 6,
+      title: 'مدیریت نظرات',
+      route: '/comment-management',
+      iconName: 'pi-comments',
+    },
+    {
+      id: 7,
+      title: 'مدیریت نقش',
+      route: '/role-management',
+      iconName:'pi-address-book',
+    },
+    {
+      id: 8,
+      title: 'مدیریت کد تخفیف',
+      route: '/discount-management',
+      iconName:'pi-percentage',
+    },
+    {
+      id: 9,
+      title: 'مدیریت تگ ها',
+      route: '/tags-management',
+      iconName:'pi-tags',
+    },
+    {
+      id: 10,
+      title: 'درباره ما',
+      route: '/about-us',
+      iconName:'pi-id-card',
     }
-  ];
-  items?: MenuItem[];
+  ]);
+  filteredItems = signal<SidebarMenu[]>(this.sidebarMenu()) ;
 
   protected readonly authService = inject(AuthService);
   private readonly router = inject(Router);
 
-  ngOnInit(): void {
-    this.items = [
-      {
-        items: [
-          {
-            label: this.authService.getUser()?.name
-          },
-          {
-            label: 'خروج',
-            icon: 'pi pi-sign-out',
-            iconStyle: {
-              color: 'red'
-            },
-            styleClass: 'logout-item',
-            command: () => {
-              console.log('click');
-              this.router.navigate(['/login']);
-              this.authService.removeUser();
-            }
-          }
-        ]
+  items = signal<MenuItem[]>([
+    {
+      label: this.authService.getUser()?.name
+    },
+    {
+      label: 'خروج',
+      icon: 'pi pi-sign-out',
+      iconStyle: {
+        color: 'red'
+      },
+      styleClass: 'logout-item',
+      command: () => {
+        console.log('click');
+        this.router.navigate(['/login']);
+        this.authService.removeUser();
       }
-    ]
+    }
+  ]);
+
+  ngOnInit(): void {
+
+    this.searchSubject.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      takeUntil(this.destroy$)
+    ).subscribe((searchTerm) => {
+      this.filteredItems.set(this.sidebarMenu()?.filter((menu) =>
+        menu.title.includes(searchTerm)
+      ))
+    });
   }
 
+  onSearch(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.searchSubject.next(value);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
